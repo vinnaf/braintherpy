49892bcd2b0a23020bc7145224122dd316336b36
vinnaf <vereslfanni@gmail.com>
2022-03-14 23:07:39 +0100

    data structure v1

--- import.py
+++ import.py
@@ -1,13 +1,24 @@
+import os
+import glob
 import pandas as pd
 
+class Experiment:
+    "this class serves to store an experiment with its date and results"
+    pass
+class Patient:
+    "this class contains all the experiments of one patient"
+    pass
 
 
+path = r'C:\Users\veres\PycharmProjects\braintherpy\data'
+csv_files = glob.glob(os.path.join(path, "*.csv"))
 
+# loop over the list of csv files
+for f in csv_files:
+    # read the csv file
+    df = pd.read_csv(f, encoding = "ISO-8859-1", low_memory=False)
 
+    # print the location and filename
+    print('Location:', f)
+    print('File Name:', f.split("\\")[-1])
 
-
-
-data = pd.read_csv('data/CB22_880218_220309132407_Small.csv', encoding = "ISO-8859-1", low_memory=False)
-
-
-print(data)
