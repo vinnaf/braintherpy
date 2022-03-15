49bf5a21edd80a33d323c1234e1f85c653670522
vinnaf <vereslfanni@gmail.com>
2022-03-15 23:22:48 +0100

    data structure v2

--- import.py
+++ import.py
@@ -1,24 +1,36 @@
 import os
 import glob
 import pandas as pd
+import csv
+
 
 class Experiment:
     "this class serves to store an experiment with its date and results"
-    pass
-class Patient:
-    "this class contains all the experiments of one patient"
+    #date, electrode type, eye, results, etc
+
+    #order by date (ascending)
+
+    #function to get data easily
+
     pass
 
+#class Patient: #first try with a single dictionary is enough
+    #"this class contains all the personal data and all the experiments of one patient"
 
+    #ID, birth date, etc, experiments
+
+    #pass
+
+
+# DATA READING
 path = r'C:\Users\veres\PycharmProjects\braintherpy\data'
 csv_files = glob.glob(os.path.join(path, "*.csv"))
-
-# loop over the list of csv files
-for f in csv_files:
-    # read the csv file
-    df = pd.read_csv(f, encoding = "ISO-8859-1", low_memory=False)
-
-    # print the location and filename
-    print('Location:', f)
-    print('File Name:', f.split("\\")[-1])
+d={}
+for f in csv_files: # loop over the list of csv files
+    df = pd.read_csv(f, encoding = "ISO-8859-1", low_memory=False, usecols=[1, 2], header=None) # read the csv file
+    df=df.values
+    df.tolist() #easier to use the table as a list of lists(rows)
+    s=df[0][1] #I tried to get the real value (for example the patient ID)
+    d[s]=df[1][1]
+print(d)
 
